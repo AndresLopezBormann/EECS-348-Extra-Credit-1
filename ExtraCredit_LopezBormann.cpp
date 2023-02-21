a2af329d935141616309db0c85141bb9c9a55c73
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2023-02-20 21:09:55 -0600

    Formatting / Editing variable names

--- ExtraCredit_LopezBormann.cpp
+++ ExtraCredit_LopezBormann.cpp
@@ -13,19 +13,17 @@ int main()
 
     while (fgets(line, sizeof(line), fp))
     {
-        sscanf(line, "%d\t%d\t%d\t%d\t%d", &input1, &input2, &input3, &input4, &input5); // Subtracting 1 so that it is 0 indexed
+        sscanf(line, "%d\t%d\t%d\t%d\t%d", &input1, &input2, &input3, &input4, &input5); 
 
-        // Department Preference
-
-        allPreferences[i][0] = input1; // Adding 5 to each input, so that the index will go from 0-9
-        allPreferences[i][1] = input2; // where dept preferences are 0-4 and student preferences will be index 5-9
+        allPreferences[i][0] = input1; 
+        allPreferences[i][1] = input2; 
         allPreferences[i][2] = input3;
         allPreferences[i][3] = input4;
         allPreferences[i][4] = input5;
         i++;
     }
 
-    SecondAlgo(allPreferences); // Gale-Shapley Algorithm
+    PreferenceAlgo(allPreferences); 
 
     fclose(fp);
     return 0;
